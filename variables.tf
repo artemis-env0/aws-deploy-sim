d87bdd5ffcc151620db7113bcf3da8f53efecfbb
artemis-env0 <artem.artyunov@env0.com>
2025-11-25 13:44:29 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,6 @@ variable "availability_zone" {
   type        = string
 }
 
-# NEW > Artem [Below] : Data volume size (in GiB) for the extra EBS volume
 variable "data_volume_size" {
   description = "Size of the additional EBS data volume in GiB."
   type        = number
